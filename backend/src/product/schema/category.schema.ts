6d7c1008295a229f42cd6c9d140ed8106c56e935
Kaung Myat Han <arkarhtethan@gmail.com>
2023-10-18 12:21:00 +0630

    update(): unique together index added

--- backend/src/product/schema/category.schema.ts
+++ backend/src/product/schema/category.schema.ts
@@ -1,15 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Types } from 'mongoose';
-import { AbstractDocument } from 'src/database/abstract.document';
+import { Document, Types } from 'mongoose';
 import { SchemaUtils } from 'src/utils/schema';
-import { Gender } from './gender.schema';
 
-@Schema()
-export class Category extends AbstractDocument {
+@Schema({ versionKey: false, collection: 'category' })
+export class Category extends Document {
   @Prop()
   name: string;
   @Prop({ type: Types.ObjectId, ref: 'gender' })
-  gender: Gender;
+  gender: string;
 }
 
 export const CategorySchema = SchemaFactory.createForClass(Category);
@@ -18,3 +16,5 @@ CategorySchema.set('toJSON', {
   virtuals: true,
   transform: SchemaUtils.transform,
 });
+
+CategorySchema.index({ name: 1, gender: 1 }, { unique: true });
