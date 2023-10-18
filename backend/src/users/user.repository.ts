30191d26e4f22c830ed6ff497997d96a60999cb7
Kaung Myat Han <arkarhtethan@gmail.com>
2023-10-18 12:21:45 +0630

    update(): remove connection from params

--- backend/src/users/user.repository.ts
+++ backend/src/users/user.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { InjectConnection, InjectModel } from '@nestjs/mongoose';
-import { Connection, Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { AbstractRepository } from 'src/database/abstract.repository';
 import { User, UserDocument } from './schemas/user.schema';
 
@@ -8,10 +8,7 @@ import { User, UserDocument } from './schemas/user.schema';
 export class UserRepository extends AbstractRepository<UserDocument> {
   protected readonly logger = new Logger(UserRepository.name);
 
-  constructor(
-    @InjectModel(User.name) orderModel: Model<UserDocument>,
-    @InjectConnection() connection: Connection,
-  ) {
-    super(orderModel, connection);
+  constructor(@InjectModel(User.name) orderModel: Model<UserDocument>) {
+    super(orderModel);
   }
 }
