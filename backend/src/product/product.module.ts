8168caef963a67417a2522c01da73eea0e620155
Kaung Myat Han <arkarhtethan@gmail.com>
2023-10-18 12:20:33 +0630

    chore(): depedencies added

---
+++ backend/src/product/product.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoryController } from './controllers/category.controller';
+import { GenderController } from './controllers/gender.controller';
+import { ProductController } from './controllers/product.controller';
+import { Category, CategorySchema } from './schema/category.schema';
+import { GenderDocument, GenderSchema } from './schema/gender.schema';
+import { Product, ProductSchema } from './schema/product.schema';
+import { CategoryService } from './services/category.service';
+import { GenderService } from './services/gender.service';
+import { ProductService } from './services/product.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Product.name, schema: ProductSchema },
+      { name: GenderDocument.name, schema: GenderSchema },
+      { name: Category.name, schema: CategorySchema },
+    ]),
+  ],
+  controllers: [GenderController, ProductController, CategoryController],
+  providers: [ProductService, GenderService, CategoryService],
+})
+export class ProductModule {}
